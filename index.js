b1a222670c36ad9c815494d6fee6418ebc42a190
phuockaito <huuphuocit1999@gmail.com>
2024-04-03 17:09:56 +0700

    cord .gitignore

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express')
 const app = express()
 const ccxt = require('ccxt');
 const moment = require('moment');
+const cors = require('cors');
+app.use(express.json());
+app.use(cors());
 
 const asciichart = require('asciichart');
 
